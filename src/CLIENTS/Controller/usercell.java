cad2b28f2f2830f3342b45493b18d7c33d67c632
aMotevalian <motevallianA99@gmail.com>
2021-06-24 01:27:07 +0430

    clean up

--- src/CLIENTS/Controller/usercell.java
+++ src/CLIENTS/Controller/usercell.java
@@ -3,16 +3,20 @@ package CLIENTS.Controller;
 import common.user;
 import javafx.scene.control.ListCell;
 
+import java.io.FileNotFoundException;
+
 public class usercell extends ListCell<user> {
     @Override
     protected void updateItem(user user, boolean empty) {
         super.updateItem(user, empty);
         if (user!=null){
-            //try {
+
+            try {
                 setGraphic(new usercellController(user).init());
-            /*} catch (IOException e) {
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
-            }*/
+            }
+
         }
     }
 }
